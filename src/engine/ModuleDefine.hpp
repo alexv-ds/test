96e98ddcb4a07b7e07a83695211c577433658b66
alexv-ds <sasichkamega@gmail.com>
2025-04-25 16:32:38 +0300

    upd

--- src/engine/ModuleDefine.hpp
+++ src/engine/ModuleDefine.hpp
@@ -7,6 +7,13 @@ namespace engine {
 
   enum class ModuleType { Standalone, /*Interface, Implementation*/ };
 
+  struct ModuleDefineStandalone {
+    std::string name{};
+    std::vector<std::string> require;
+    std::vector<std::string> optional;
+    std::function<void(ServiceRegistry&)> init = nullptr;
+  };
+
   struct ModuleDefine {
     ModuleType type = ModuleType::Standalone;
     std::string name{};
@@ -14,6 +21,16 @@ namespace engine {
     std::vector<std::string> require; // dependencies
     std::vector<std::string> optional; // optional dependencies
     std::function<void(ServiceRegistry&)> init = nullptr;
+
+    ModuleDefine() = default;
+    // ReSharper disable once CppNonExplicitConvertingConstructor
+    ModuleDefine(ModuleDefineStandalone define) { // NOLINT(*-explicit-constructor)
+      this->type = ModuleType::Standalone;
+      this->name = std::move(define.name);
+      this->require = std::move(define.require);
+      this->optional = std::move(define.optional);
+      this->init = std::move(define.init);
+    }
   };
 
 
--- src/engine/modules/sound/define.cpp
+++ src/engine/modules/sound/define.cpp
@@ -4,9 +4,10 @@ namespace engine::sound {
 
   const std::string module_name = "engine::sound";
 
-  const ModuleDefine module_define = {
-    .type = ModuleType::Standalone,
-    .name = module_name
+  const ModuleDefine module_define = ModuleDefineStandalone{
+    .name = module_name,
+    .require = {},
+    .optional = {}
   };
 
 
